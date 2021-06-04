e5d1bde294e671bc7805147c9ae0a74d46dc29dc
symdmr <seymademir35@gmail.com>
2021-06-04 22:31:31 +0300

    console_commit

---
+++ console-programlama1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; //kullanılcak kütüphaneleri vs yazıyorsun, bunu silersen aşağıda console.writeline gibi komutların başına hep system yamak zorunda kalırsın
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args) //main metot programın başladığı ve bittiği yer. ilk satırla çalışır son satırla biter
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba, " + name + " " + surname);
+
+
+        }
+    }
+}
