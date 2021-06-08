7174f4f2d0ba6b0493d31a6a96471d00e0b13de5
symdmr <seymademir35@gmail.com>
2021-06-08 14:03:54 +0300

    metot-overloading_commit

---
+++ metot_overloading/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace metot_overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //out parametreler
+            string sayi = "999";
+
+            bool sonuc  =int.TryParse(sayi, out int outSayi);
+            if(sonuc){
+                Console.WriteLine("başarılı");
+                Console.WriteLine(outSayi);
+            }
+            else{
+                Console.WriteLine("başarısız");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(4,5, out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+
+            //metot overload/aşırı yükleme
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("sym","dmr");
+
+            //metot imzası
+            //metotAdı = parametresayısı + parametre
+
+        }
+    }
+
+    class Metotlar{
+        public void Topla(int a, int b, out int toplam){
+            toplam =a+b;
+        }
+
+        public void EkranaYazdir(string veri){
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdir(int veri){
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdir(string veri1, string veri2){
+            Console.WriteLine(veri1+ veri2);
+        }
+    }
+}
