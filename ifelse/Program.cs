293287a1c7be102573a6f3eea74d2c94e1f35a44
symdmr <seymademir35@gmail.com>
2021-06-08 10:31:55 +0300

    ifelse_commit

---
+++ ifelse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time =DateTime.Now.Hour;
+            if (time >=6 && time<11)
+            {
+                Console.WriteLine("günaydın");                
+            }
+            else if(time<=18){
+                Console.WriteLine("iyi günler");
+            }
+            else{
+                Console.WriteLine("iyi geceler");
+            }
+
+            string sonuc  = time<=18 ? "iyi günler" : "iyi geceler";
+
+            sonuc =time>= 6 && time<11 ? "günaydın" : time<=18? "iyi günler" : "iyi geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
