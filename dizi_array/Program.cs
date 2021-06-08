36b8771e2bf081bf52308c3d662679166cff47a9
symdmr <seymademir35@gmail.com>
2021-06-08 12:20:44 +0300

    dizi_array_commit

---
+++ dizi_array/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace dizi_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sort
+            int[] sayıDizisi = {25,12, 45, 56, 85, 26, 28,2};
+
+            //sırasız dizi
+            foreach (var sayi in sayıDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            
+            //sıralı dizi
+            Array.Sort(sayıDizisi); 
+
+            foreach (var sayi in sayıDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //clear
+            //sayı dizisi elemanlarını kullanarak 2. index ten itibaren 2 tane elemanı sıfırlar
+            Array.Clear(sayıDizisi, 2, 2);
+            foreach (var sayi in sayıDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+
+            //reverse
+            Array.Reverse(sayıDizisi);
+            foreach (var sayi in sayıDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //indexOf
+            Console.WriteLine(Array.IndexOf(sayıDizisi,25));
+
+            //resize
+            Array.Resize<int>(ref sayıDizisi,9);
+            sayıDizisi[8] = 99;
+
+            foreach (var sayi in sayıDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+        }
+    }
+}
